ca97528c2b888dc85ff6910946cf0a4969fa72d0
Pawan Mandhan <pawmandh@publicisgroupe.net>
2020-08-28 03:32:25 +0530

    LL class structure

---
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -0,0 +1,16 @@
+package com.linkedlist;
+
+public class LinkedList {
+	
+	private class Node {
+		int data;
+		Node next;
+	}
+	
+	private Node head;
+	private Node tail;
+	private int size;
+	
+	
+
+}
