19bcc5c5da97a6a4d63d4763debf197265e33129
Pawan Mandhan <pawmandh@publicisgroupe.net>
2020-08-28 03:36:43 +0530

    LL display

--- src/main/java/com/linkedlist/LinkedList.java
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -1,16 +1,22 @@
 package com.linkedlist;
 
 public class LinkedList {
-	
+
 	private class Node {
 		int data;
 		Node next;
 	}
-	
+
 	private Node head;
 	private Node tail;
 	private int size;
-	
-	
+
+	public void display() {
+		Node temp = this.head;
+		while (temp.next != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
 
 }
