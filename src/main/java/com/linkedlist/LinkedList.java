0953fa1b85155cb2e00647266d4d2169e52a10e7
Pawan Mandhan <pawmandh@publicisgroupe.net>
2020-08-28 03:53:29 +0530

    LL add at tail

--- src/main/java/com/linkedlist/LinkedList.java
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -5,6 +5,14 @@ public class LinkedList {
 	private class Node {
 		int data;
 		Node next;
+
+		Node() {
+		}
+
+		Node(int data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
 
 	private Node head;
@@ -13,10 +21,20 @@ public class LinkedList {
 
 	public void display() {
 		Node temp = this.head;
-		while (temp.next != null) {
-			System.out.println(temp.data + " ");
+		while (temp != null) {
+			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
+	}
+
+	public void addLast(int item) {
+		// create a new node
+		Node newNode = new Node(item, null);
+		if (this.size == 0) this.head = newNode;
+		else this.tail.next = newNode;
+
+		this.tail = newNode;
+		this.size++;
 	}
 
 }
